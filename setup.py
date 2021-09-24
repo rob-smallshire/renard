1f41b88719ab80359dd28becad29200b4e960b17
Robert Smallshire <robert@smallshire.org.uk>
2021-09-24 21:09:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['docopt_subcommands'],
+    install_requires=['docopt_subcommands>=4.0'],
 
     # List additional groups of dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
